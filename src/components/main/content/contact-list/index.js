8e5ecac8f9ba67b5ff8bcebaf4dfe991bc639472
Alex M <amirtcheva@mac-mini.local>
2017-09-29 03:12:22 -0700

    styling for more

--- src/components/main/content/contact-list/index.js
+++ src/components/main/content/contact-list/index.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './contact-list.scss';
 
 export default class ContactList extends React.Component {
   constructor(props) {
     super(props);
-    console.log("entering");
   }
 
   shouldComponentUpdate(nextProps) {
-    if(nextProps.name != this.props.name) return true;
+    if(nextProps.contacts != this.props.contacts) return true;
+    if(nextProps.expanded != this.props.expanded) return true;
     return false;
+  }
+
+  displayDefault(name, id) { // default display for contact
+    return (<li key={name+id}>
+      {name}
+      <div className="more" onClick={this.onClickMore(id)}>
+        More...
+      </div>
+    </li>);
+  }
+
+  displayExpanded(contact) {
+    // const name, email, phoneNumber, address, company, birthday, image;
+    return (<li key={contact.name+contact.id}>
+      {contact.email}
+    </li>)
+  }
+
+  onClickMore(id){
+    //call expand contact action to update store state
+    console.log(id);
   }
 
   render() {
     return (
-     <div>
-        Welcome, Alex
+     <div className="contact-list-container">
+       <ul>
+         {this.props.contacts.people.map(function(contact){
+           return this.props.expanded == contact.id ? this.displayExpanded(contact) :
+              this.displayDefault(contact.name, contact.id)
+         }, this)}
+       </ul>
       </div>
       );
   }
 }
 
-Header.propTypes = {
+ContactList.propTypes = {
   name: PropTypes.string
 };
--- src/components/main/content/index.js
+++ src/components/main/content/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ContactList from './contact-list';
 import './content.scss';
 
 export default class Content extends React.Component {
@@ -16,6 +17,7 @@ export default class Content extends React.Component {
     return (
      <div className="content-container">
         <h6>Contacts</h6>
+        <ContactList {...this.props} />
       </div>
     );
   }
